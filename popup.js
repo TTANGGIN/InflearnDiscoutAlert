76a6fb7f20c947fcb5893296c925ad422c0de51c
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-02 13:53:29 +0900

    feat: 강의별명 입력, 강의 삭제 기능 추가

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 document.getElementById("addUrl").addEventListener("click", function () {
   const url = document.getElementById("courseUrl").value;
-  if (url.startsWith("https://www.inflearn.com/course/")) {
+  const alias = document.getElementById("courseAlias").value;
+
+  if (url.startsWith("https://www.inflearn.com/course/") && alias) {
     chrome.storage.local.get("courses", function (data) {
       let courses = data.courses || [];
-      if (courses.indexOf(url) === -1) {
-        courses.push(url);
+      let courseObj = { url: url, alias: alias };
+      if (!courses.some((course) => course.url === url)) {
+        courses.push(courseObj);
         chrome.storage.local.set({ courses: courses }, function () {
           populateCourseList();
         });
@@ -18,10 +21,27 @@ function populateCourseList() {
     let courses = data.courses || [];
     let listEl = document.getElementById("courseList");
     listEl.innerHTML = "";
+
     courses.forEach((course) => {
       let li = document.createElement("li");
-      li.textContent = course;
+      let deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "X";
+      deleteBtn.onclick = function () {
+        deleteCourse(course.url);
+      };
+      li.textContent = course.alias;
+      li.appendChild(deleteBtn);
       listEl.appendChild(li);
+    });
+  });
+}
+
+function deleteCourse(url) {
+  chrome.storage.local.get("courses", function (data) {
+    let courses = data.courses || [];
+    courses = courses.filter((course) => course.url !== url);
+    chrome.storage.local.set({ courses: courses }, function () {
+      populateCourseList();
     });
   });
 }
